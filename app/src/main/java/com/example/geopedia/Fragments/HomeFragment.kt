e85113a135580f88cf480bf0477602ed81b7d56d
Yashvant Yadav <yashvantyadav855@gmail.com>
2023-10-07 18:57:09 +0530

    changing home fragment

--- app/src/main/java/com/example/geopedia/Fragments/HomeFragment.kt
+++ app/src/main/java/com/example/geopedia/Fragments/HomeFragment.kt
@@ -12,16 +12,6 @@ import com.example.geopedia.R
 import com.example.geopedia.databinding.FragmentHomeBinding
 import com.google.android.material.button.MaterialButton
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
     lateinit var binding: FragmentHomeBinding
 
