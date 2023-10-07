cd6612ce1108e7dfaff614ac4d3c15fba2b10ab9
Yashvant Yadav <yashvantyadav855@gmail.com>
2023-10-07 17:36:03 +0530

    markers can be added on long click

--- app/src/main/java/com/example/geopedia/Fragments/MapsFragment.kt
+++ app/src/main/java/com/example/geopedia/Fragments/MapsFragment.kt
@@ -12,15 +12,18 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import java.util.Locale
 
 class MapsFragment : Fragment() {
 
     private val callback = OnMapReadyCallback { googleMap ->
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        val sydney = LatLng(28.535517, 77.391029)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Noida"))
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        setMapLongClick(googleMap)
     }
 
     override fun onCreateView(
@@ -35,5 +38,25 @@ class MapsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+    }
+
+    private fun setMapLongClick(map: GoogleMap) {
+        map.setOnMapLongClickListener { latLng ->
+            // A Snippet is Additional text that's displayed below the title.
+            val snippet = String.format(
+                Locale.getDefault(),
+                "Lat: %1$.5f, Long: %2$.5f",
+                latLng.latitude,
+                latLng.longitude
+            )
+            map.addMarker(
+                MarkerOptions()
+                    .position(latLng)
+                    .title("Marked location")
+                    .snippet(snippet)
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+//                    .draggable(true)
+            )
+        }
     }
 }
